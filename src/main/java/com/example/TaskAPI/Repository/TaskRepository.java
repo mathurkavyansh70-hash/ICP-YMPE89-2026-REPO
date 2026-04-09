cce85bdc1848d60e1f7cf4de30bd80be82860e11
Kavyansh Mathur <mathur.kavyansh70@gmail.com>
2026-04-09 17:11:50 +0530

    Delete src/main directory

--- src/main/java/com/example/TaskAPI/Repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TaskAPI.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TaskAPI.Entity.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
