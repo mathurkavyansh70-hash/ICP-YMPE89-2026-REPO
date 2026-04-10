d7f879fe72953d5368037d03e71194ab6831ed54
Kavyansh Mathur <mathur.kavyansh70@gmail.com>
2026-04-10 16:17:54 +0530

    Delete src directory

--- src/main/src/main/java/com/example/DataJpaAPI/Repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DataJpaAPI.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.DataJpaAPI.Entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- src/main/src/main/java/com/example/DataJpaAPI/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DataJpaAPI.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.DataJpaAPI.Entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
